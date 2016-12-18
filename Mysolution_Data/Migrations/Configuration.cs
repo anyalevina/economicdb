d338e0390fb13c7c4d29d5c6f5d9dbf8ed17cffa
игорь <@DESKTOP-UVQKOOO>
2016-12-18 11:45:28 +0300

    Some fixes to the Seed method

--- Mysolution_Data/Migrations/Configuration.cs
+++ Mysolution_Data/Migrations/Configuration.cs
@@ -32,24 +32,24 @@ namespace Mysolution_Data.Migrations
                     context.country.AddOrUpdate(c => c.IdCountry, countries);
                 }
             }
-            //resourceName = "Mysolution_Data.Files.export.csv";
-            //using (Stream stream = assembly.GetManifestResourceStream(resourceName))
-            //{
-            //    using (StreamReader reader = new StreamReader(stream))
-            //    {
-            //        CsvReader csvReader = new CsvReader(reader);
-            //        csvReader.Configuration.Delimiter = ";";
-            //        csvReader.Configuration.WillThrowOnMissingField = false;
-            //        while (csvReader.Read())
-            //        {
-            //            var myexport = csvReader.GetRecord<export>();
-            //            var countryCode = csvReader.GetField<int>("country_IdCountry");
-            //            myexport.country = context.country.Local.Single(c => c.IdCountry == countryCode);
-            //            context.export.AddOrUpdate(p => p.Year, myexport);
+            resourceName = "Mysolution_Data.Files.export.csv";
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    CsvReader csvReader = new CsvReader(reader);
+                    csvReader.Configuration.Delimiter = ";";
+                    csvReader.Configuration.WillThrowOnMissingField = false;
+                    while (csvReader.Read())
+                    {
+                        var myexport = csvReader.GetRecord<export>();
+                        var countryCode = csvReader.GetField<int>("country_IdCountry");
+                        myexport.country = context.country.Local.Single(c => c.IdCountry == countryCode);
+                        context.export.AddOrUpdate(myexport);
 
-            //        }
-            //    }
-            //}
+                    }
+                }
+            }
             resourceName = "Mysolution_Data.Files.gdp.csv";
             using (Stream stream = assembly.GetManifestResourceStream(resourceName))
             {
@@ -63,112 +63,112 @@ namespace Mysolution_Data.Migrations
                         var myexport = csvReader.GetRecord<gdp>();
                         var countryCode = csvReader.GetField<int>("country_IdCountry");
                         myexport.country = context.country.Local.Single(c => c.IdCountry == countryCode);
-                        context.gdp.AddOrUpdate(p=>p.Year,myexport);
+                        context.gdp.AddOrUpdate(myexport);
                     }
                 }
             }
-            //resourceName = "Mysolution_Data.Files.gdpgrowth.csv";
-            //using (Stream stream = assembly.GetManifestResourceStream(resourceName))
-            //{
-            //    using (StreamReader reader = new StreamReader(stream))
-            //    {
-            //        CsvReader csvReader = new CsvReader(reader);
-            //        csvReader.Configuration.Delimiter = ";";
-            //        csvReader.Configuration.WillThrowOnMissingField = false;
-            //        while (csvReader.Read())
-            //        {
-            //            var mygdpgrowth = csvReader.GetRecord<gdpgrowth>();
-            //            var countryCode = csvReader.GetField<int>("country_IdCountry");
-            //            mygdpgrowth.country = context.country.Local.Single(c => c.IdCountry == countryCode);
-            //            context.gdpgrowth.AddOrUpdate(mygdpgrowth);
-            //        }
-            //    }
-            //}
-            //resourceName = "Mysolution_Data.Files.import.csv";
-            //using (Stream stream = assembly.GetManifestResourceStream(resourceName))
-            //{
-            //    using (StreamReader reader = new StreamReader(stream))
-            //    {
-            //        CsvReader csvReader = new CsvReader(reader);
-            //        csvReader.Configuration.Delimiter = ";";
-            //        csvReader.Configuration.WillThrowOnMissingField = false;
-            //        while (csvReader.Read())
-            //        {
-            //            var myimport = csvReader.GetRecord<import1>();
-            //            var countryCode = csvReader.GetField<int>("country_IdCountry");
-            //            myimport.country = context.country.Local.Single(c => c.IdCountry == countryCode);
-            //            context.import1.AddOrUpdate(myimport);
-            //        }
-            //    }
-            //}
-            //resourceName = "Mysolution_Data.Files.inflation.csv";
-            //using (Stream stream = assembly.GetManifestResourceStream(resourceName))
-            //{
-            //    using (StreamReader reader = new StreamReader(stream))
-            //    {
-            //        CsvReader csvReader = new CsvReader(reader);
-            //        csvReader.Configuration.Delimiter = ";";
-            //        csvReader.Configuration.WillThrowOnMissingField = false;
-            //        while (csvReader.Read())
-            //        {
-            //            var myinflation = csvReader.GetRecord<inflation>();
-            //            var countryCode = csvReader.GetField<int>("country_IdCountry");
-            //            myinflation.country = context.country.Local.Single(c => c.IdCountry == countryCode);
-            //            context.inflation.AddOrUpdate(myinflation);
-            //        }
-            //    }
-            //}
-            //resourceName = "Mysolution_Data.Files.natdebt.csv";
-            //using (Stream stream = assembly.GetManifestResourceStream(resourceName))
-            //{
-            //    using (StreamReader reader = new StreamReader(stream))
-            //    {
-            //        CsvReader csvReader = new CsvReader(reader);
-            //        csvReader.Configuration.Delimiter = ";";
-            //        csvReader.Configuration.WillThrowOnMissingField = false;
-            //        while (csvReader.Read())
-            //        {
-            //            var mynatdebt = csvReader.GetRecord<national_debt>();
-            //            var countryCode = csvReader.GetField<int>("country_IdCountry");
-            //            mynatdebt.country = context.country.Local.Single(c => c.IdCountry == countryCode);
-            //            context.national_debt.AddOrUpdate(mynatdebt);
-            //        }
-            //    }
-            //}
-            //resourceName = "Mysolution_Data.Files.population.csv";
-            //using (Stream stream = assembly.GetManifestResourceStream(resourceName))
-            //{
-            //    using (StreamReader reader = new StreamReader(stream))
-            //    {
-            //        CsvReader csvReader = new CsvReader(reader);
-            //        csvReader.Configuration.Delimiter = ";";
-            //        csvReader.Configuration.WillThrowOnMissingField = false;
-            //        while (csvReader.Read())
-            //        {
-            //            var mypopulation = csvReader.GetRecord<population>();
-            //            var countryCode = csvReader.GetField<int>("country_IdCountry");
-            //            mypopulation.country = context.country.Local.Single(c => c.IdCountry == countryCode);
-            //            context.population.AddOrUpdate(mypopulation);
-            //        }
-            //    }
-            //}
-            //resourceName = "Mysolution_Data.Files.unemployment.csv";
-            //using (Stream stream = assembly.GetManifestResourceStream(resourceName))
-            //{
-            //    using (StreamReader reader = new StreamReader(stream))
-            //    {
-            //        CsvReader csvReader = new CsvReader(reader);
-            //        csvReader.Configuration.Delimiter = ";";
-            //        csvReader.Configuration.WillThrowOnMissingField = false;
-            //        while (csvReader.Read())
-            //        {
-            //            var myunemployment = csvReader.GetRecord<unemployment>();
-            //            var countryCode = csvReader.GetField<int>("country_IdCountry");
-            //            myunemployment.country = context.country.Local.Single(c => c.IdCountry == countryCode);
-            //            context.unemployment.AddOrUpdate(myunemployment);
-            //        }
-            //    }
-            //}
+            resourceName = "Mysolution_Data.Files.gdpgrowth.csv";
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    CsvReader csvReader = new CsvReader(reader);
+                    csvReader.Configuration.Delimiter = ";";
+                    csvReader.Configuration.WillThrowOnMissingField = false;
+                    while (csvReader.Read())
+                    {
+                        var mygdpgrowth = csvReader.GetRecord<gdpgrowth>();
+                        var countryCode = csvReader.GetField<int>("country_IdCountry");
+                        mygdpgrowth.country = context.country.Local.Single(c => c.IdCountry == countryCode);
+                        context.gdpgrowth.AddOrUpdate(mygdpgrowth);
+                    }
+                }
+            }
+            resourceName = "Mysolution_Data.Files.import.csv";
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    CsvReader csvReader = new CsvReader(reader);
+                    csvReader.Configuration.Delimiter = ";";
+                    csvReader.Configuration.WillThrowOnMissingField = false;
+                    while (csvReader.Read())
+                    {
+                        var myimport = csvReader.GetRecord<import1>();
+                        var countryCode = csvReader.GetField<int>("country_IdCountry");
+                        myimport.country = context.country.Local.Single(c => c.IdCountry == countryCode);
+                        context.import1.AddOrUpdate(myimport);
+                    }
+                }
+            }
+            resourceName = "Mysolution_Data.Files.inflation.csv";
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    CsvReader csvReader = new CsvReader(reader);
+                    csvReader.Configuration.Delimiter = ";";
+                    csvReader.Configuration.WillThrowOnMissingField = false;
+                    while (csvReader.Read())
+                    {
+                        var myinflation = csvReader.GetRecord<inflation>();
+                        var countryCode = csvReader.GetField<int>("country_IdCountry");
+                        myinflation.country = context.country.Local.Single(c => c.IdCountry == countryCode);
+                       context.inflation.AddOrUpdate(myinflation);
+                    }
+                }
+            }
+            resourceName = "Mysolution_Data.Files.natdebt.csv";
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    CsvReader csvReader = new CsvReader(reader);
+                    csvReader.Configuration.Delimiter = ";";
+                    csvReader.Configuration.WillThrowOnMissingField = false;
+                    while (csvReader.Read())
+                    {
+                        var mynatdebt = csvReader.GetRecord<national_debt>();
+                        var countryCode = csvReader.GetField<int>("country_IdCountry");
+                        mynatdebt.country = context.country.Local.Single(c => c.IdCountry == countryCode);
+                       context.national_debt.AddOrUpdate(mynatdebt);
+                    }
+                }
+            }
+            resourceName = "Mysolution_Data.Files.population.csv";
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    CsvReader csvReader = new CsvReader(reader);
+                    csvReader.Configuration.Delimiter = ";";
+                    csvReader.Configuration.WillThrowOnMissingField = false;
+                    while (csvReader.Read())
+                    {
+                        var mypopulation = csvReader.GetRecord<population>();
+                        var countryCode = csvReader.GetField<int>("country_IdCountry");
+                        mypopulation.country = context.country.Local.Single(c => c.IdCountry == countryCode);
+                        context.population.AddOrUpdate(mypopulation);
+                    }
+                }
+            }
+            resourceName = "Mysolution_Data.Files.unemployment.csv";
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                   CsvReader csvReader = new CsvReader(reader);
+                    csvReader.Configuration.Delimiter = ";";
+                    csvReader.Configuration.WillThrowOnMissingField = false;
+                    while (csvReader.Read())
+                   {
+                        var myunemployment = csvReader.GetRecord<unemployment>();
+                        var countryCode = csvReader.GetField<int>("country_IdCountry");
+                        myunemployment.country = context.country.Local.Single(c => c.IdCountry == countryCode);
+                        context.unemployment.AddOrUpdate(myunemployment);
+                    }
+                }
+            }
             //This method will be called after migrating to the latest version.
 
             //You can use the DbSet<T>.AddOrUpdate() helper extension method
